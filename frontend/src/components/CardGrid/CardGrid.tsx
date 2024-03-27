5b503be68144dcf8a5d27f7da32165adb3d76cd8
Alex Russell <alexrusselldev@gmail.com>
2024-03-27 12:22:53 +0000

    feat: ✨ add cardgrid component

---
+++ frontend/src/components/CardGrid/CardGrid.tsx
@@ -0,0 +1,36 @@
+import { ImageWithFallback } from "../ImageWithFallback";
+import Link from "next/link";
+
+interface IProps {
+  items: {
+    slug: string;
+    title: string;
+    image: {
+      src: string;
+      alt: string;
+    };
+  }[];
+}
+
+export const CardGrid: React.FC<IProps> = (props) => {
+  const { items } = props;
+  return (
+    <div className="mx-auto max-w-md sm:max-w-xl md:max-w-3xl">
+      <ul className="flex grid-cols-2 flex-col gap-4 sm:grid md:grid-cols-3">
+        {items.map(({ slug, title, image }) => {
+          return (
+            <li key={slug}>
+              <Link
+                href={`/blog/${slug}`}
+                className="flex flex-col items-center"
+              >
+                <ImageWithFallback src={image.src} alt={image.alt} />
+                <span className="text-xl">{title}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
