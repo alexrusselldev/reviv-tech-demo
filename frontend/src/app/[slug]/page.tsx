befcb46f1e0c1d665a70b637ac4f3a82445e020f
Alex Russell <alexrusselldev@gmail.com>
2024-03-27 10:15:24 +0000

    fix: 🐛 get slug properly

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -2,16 +2,16 @@ import { MDXRemoteWrapper } from "@/components/MDXRemoteWrapper";
 import { getPage } from "@/lib/strapi";
 import { NextPage } from "next";
 import { serialize } from "next-mdx-remote/serialize";
-import { headers } from "next/headers";
-import { useRouter } from "next/router";
 
-interface IProps {}
+interface IProps {
+  params: {
+    slug: string;
+  };
+}
 
-const Page: NextPage<IProps> = async () => {
-  const pathname = headers().get("next-url") || "";
-  const path = pathname.substring(1, pathname.length + 1).split("/")[0];
-
-  const pageData = await getPage(path);
+const Page: NextPage<IProps> = async (props) => {
+  const { params } = props;
+  const pageData = await getPage(params.slug);
 
   const content = await serialize(pageData.attributes.content);
 
